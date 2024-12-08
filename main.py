2fe69fcbea4d7bb3d8e3b126d5034cf25e9e54a8
MaxSchwein <max.schweinberger@live.at>
2024-12-08 21:11:56 +0100

    Update main.py
    Tutorial Code gefixed, sprachlich auf Deutsch angepasst.
    Bitte gegencheck machen!

--- main.py
+++ main.py
@@ -1,72 +1,80 @@
-#Erstellung des Decks
+# Pack zum Mischen der Karten
 import random
 
+
+# Kartendeck und Werte
 card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
 deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
+
+# Deck mischen
 random.shuffle(deck)
 
-#Austeilen der Karten und Kalkulation der Karten
+# Oberste Karte vom Deck ziehen
 def deal_card(deck):
     return deck.pop()
 
+# Hand zählen
 def calculate_total(hand):
     total = sum(card_values[card] for card in hand)
-    # Anspassung für Asse
+    # Ass als 1 ausgeben wenn Busted
     for card in hand:
         if card == 'A' and total > 21:
             total -= 10
     return total
 
-#Zug des Spielers
+# Spielzug Spieler
 def player_turn(deck, player_hand):
     while True:
-        player_choice = input("Hit or Stand? ").lower()
-        if player_choice == 'hit':
+        player_choice = input("Ziehen oder Bleiben? ").lower()
+        if player_choice == 'ziehen':
             player_hand.append(deal_card(deck))
             total = calculate_total(player_hand)
-            print("Your hand:", player_hand, "Total:", total)
+            print("Deine Hand:", player_hand, "Summe:", total)
             if total > 21:
-                print("Bust! You lost.")
+                print("Oh Nein! Du hast verloren. Dein Geld gehört mir!")
                 return False
-        elif player_choice == 'stand':
+        elif player_choice == 'bleiben':
             return True
 
-#Zug des Dealers    
+# Spielzug Dealer
 def dealer_turn(deck, dealer_hand):
     while calculate_total(dealer_hand) < 17:
         dealer_hand.append(deal_card(deck))
     return dealer_hand
 
-#Das Spiel spielen
+# Blackjack Spiel
 while True:
     player_hand = [deal_card(deck), deal_card(deck)]
     dealer_hand = [deal_card(deck), deal_card(deck)]
     
-    print("Your hand:", player_hand, "Total:", calculate_total(player_hand))
-    print("Dealer's hand:", dealer_hand[0])
-   # Player's Turn
-    if player_turn(deck, player_hand):
-       dealer_hand = dealer_turn(deck, dealer_hand)
-       print("Dealer's hand:", dealer_hand, "Total:", calculate_total(dealer_hand))
-       
-       # Determine Winner
-       player_total = calculate_total(player_hand)
-       dealer_total = calculate_total(dealer_hand)
-    if dealer_total > 21 or player_total > dealer_total:
-        print("You win!")
-    elif dealer_total > player_total:
-        print("Dealer wins!")
-    else:
-        print("It's a tie!")
+    print("Deine Hand:", player_hand, "Summe:", calculate_total(player_hand))
+    print("Dealer's Hand:", dealer_hand[0])
 
-   # Check if player wants to play again
-play_again = input("Play again? (yes/no): ").lower()
-    if play_again != 'yes':
+    # Spieler am Zug
+    if player_turn(deck, player_hand):
+        dealer_hand = dealer_turn(deck, dealer_hand)
+        print("Dealer's Hand:", dealer_hand, "Summe:", calculate_total(dealer_hand))
+       
+        # Gewinner festlegen
+        player_total = calculate_total(player_hand)
+        dealer_total = calculate_total(dealer_hand)
+        if player_total > dealer_total:
+            print("Du hast gewonnen!")
+        elif dealer_total > 21:
+            print("Dealer hat sich überkauft! Du gewinnst!")
+        elif dealer_total > player_total:
+            print("Dealer gewinnt!")
+        else:
+            print("Unentschieden!")
+
+    # Zweite Runde anbieten
+    play_again = input("Nochmal spielen? (Ja/Nein): ").lower()
+    if play_again != 'ja':
         break
 
-   # Re-shuffle the deck if it's running low
-if len(deck) < 10:
-       deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
-       random.shuffle(deck)
+    # Deck neu anlegen und mischen
+    if len(deck) < 10:
+        deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
+        random.shuffle(deck)
 
-print("Thanks for playing!")
+print("Danke fürs mitspielen!")
